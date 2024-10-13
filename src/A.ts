9ad313583b34e757be48f7f5c41567267817f4de
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-13 21:44:31 +0900

    호호

--- src/A.ts
+++ src/A.ts
@@ -1,19 +1,24 @@
-let count = 0; //숫자
-count += 1;
-// count = '갑자기 분위기 문자열'   ~~~코드가 이래버리면 에러가 터집니다 ㅋㅋ
+let hello: string = "helloWorld!";
 
-const message :string = 'Hello World'; // 문자열
 
-const done :boolean = true; // 불리언 값 
 
-const numbers :number[] = [1, 2, 3]; // 숫자 배열
-const stringArray :string[] = ['Hello', 'World']; // 문자열 배열
+let tripleSeven: number = 777;
 
-// stringArray.push(1);   ~~~문자열 배열에 숫자를 넣으려 하다닛 ㅠㅠ 안댐
 
-let mightBeUndifined :string | undefined = undefined; // string 일수도 있고 undifined 일수도 있고
-let nullAbleNumber :number | null = null; // number 일수도 있고 null 일수도 있고
 
-let color :'red' | 'orange' | 'yellow' = 'red'; // red, orange, yellow 중 하나임
-color = 'yellow';
-// color = 'green'   ~~~에러 터진다 임마 ...
+let arr1: number[] = [10, 20, 30];
+let arr2: Array<number> = [10 , 20, 30];
+let arr3: Array<string> = ["hello", "world"];
+let arr4: [string, number] = ["jinyoung", 24];
+
+
+
+let human: object = { name: "jinyoung", age: 24 };
+let person: { name: string; age: number } = {
+  name: "jinyoung",
+  age: 24
+};
+
+
+
+let isThatTrue: boolean = true;
--- src/B.ts
+++ src/B.ts
@@ -1,12 +1,16 @@
-// 함수에서 타입 정의하는 방법
-// 타스를 쓰면 코드 작성 과정에서 함수의 파라미터로 어떤 타입을 넣어야 하는지 알 수 있으며 리턴 값의 타입도 정의함
-// 만약에 return 값이 number라는 것을 정의 해놓고 갑자기 null값을 반환한다면 오류 발생
-function sum(x :number, y :number) :number {
+function add(x: number, y: number): number {
     return x + y;
 }
 
-// 숫자 배열의 총합을 구하는 sumArray 함수
-function sumArray(numbers :number[]) :number {
-    return numbers.reduce((acc, current) => acc + current, 0);
+
+
+function buildName(firstName: string, lastName?: string) {
+    if (lastName)
+        return firstName + " " + lastName;
+    else
+        return firstName;
 }
-const total= sumArray([1, 2, 3, 4, 5]);
+
+let result1 = buildName("Bob"); // 문제 없음
+// let result2 = buildName("Bob", "Adams", "Sr.");   에러 발생
+let result3 = buildName("Bob", "Adams"); // 문제 없음
