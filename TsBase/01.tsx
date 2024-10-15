0a45ec8b4d4282f30bbe209a2f368a202e8108f7
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-15 15:23:56 +0900

    굳

--- TsBase/01.tsx
+++ TsBase/01.tsx
@@ -1,19 +1,15 @@
-// 문자열
 let hello: string = "helloWorld!";
 
 
-// 숫자
 let tripleSeven: number = 777;
 
 
-// 배열
 let arr1: number[] = [10, 20, 30];
 let arr2: Array<number> = [10 , 20, 30];
 let arr3: Array<string> = ["hello", "world"];
 let arr4: [string, number] = ["jinyoung", 24];
 
 
-// 객체
 let human: object = { name: "jinyoung", age: 24 };
 let person: { name: string; age: number } = {
   name: "jinyoung",
@@ -21,5 +17,4 @@ let person: { name: string; age: number } = {
 };
 
 
-// 불리언
 let isThatTrue: boolean = false;
